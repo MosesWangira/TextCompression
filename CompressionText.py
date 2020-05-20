9a6eccd9525889dabbb1f24d766e75ebc1a35282
Moses Ouma Wangira <40572173+MosesWangira@users.noreply.github.com>
2020-05-20 12:11:48 +0300

    Source code

---
+++ CompressionText.py
@@ -0,0 +1,17 @@
+#Python script for compression of test using lossy method
+#Not advisable for texts but I use it as a stepping stone
+TextToCompress = input("Enter your sentence : ") 
+
+def compression(original):
+    result = ""
+    for i in range(len(original)):
+        charactor = original[i]
+        if(charactor not in ('a','e','i','o','u')):
+           result += charactor
+    print(result)
+print("*********Compressing******")
+compressed = compression(TextToCompress)
+
+#example
+#Input : Hey I love you dont leave me
+#Output : Hy I lv y dnt lv m
